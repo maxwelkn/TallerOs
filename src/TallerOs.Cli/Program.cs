6693d8656dc0c14ca419ed475ee50ef57bb1f1f6
Maxwel Kenen Sael Soriano <saelmaxwel@gmail.com>
2026-09-21 21:55:45 -0400

    chore: add .NET ignore and welcome function

---
+++ src/TallerOs.Cli/Program.cs
@@ -0,0 +1,14 @@
+namespace TallerOs.Cli;
+
+public static class Program
+{
+    public static void Main()
+    {
+        MostrarBienvenida();
+    }
+
+    private static void MostrarBienvenida()
+    {
+        Console.WriteLine("Bienvenido a TallerOs");
+    }
+}
